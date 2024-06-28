ede6f9782926107cfb803eab20c2c731cd30461b
STemple <sean.temple.dev@gmail.com>
2024-06-28 16:45:03 -0600

    More text conversion functions

--- src/main.py
+++ src/main.py
@@ -7,11 +7,16 @@ This is an *italic and **bold** word*.
 For simplicity's sake, we won't allow it! We will only support a single level of nesting when it comes to inline elements. If you want to extend the project to support multiple levels of nesting, you're welcome to do so! We just won't be covering it explicitly.
 """
 
-from textnode import TextNode
-
-test = TextNode("This is a node", "bol", "url")
-test2 = 3 
-
-print(test == test2)
-
-print(test)
+from textnode import * 
+node = TextNode(
+    "This is text with a [link](https://boot.dev) and [another link](https://blog.boot.dev) with text that follows",
+    text_type_text, )
+new_nodes = split_nodes_link([node])
+print([
+    TextNode("This is text with a ", text_type_text),
+    TextNode("link", text_type_link, "https://boot.dev"),
+    TextNode(" and ", text_type_text),
+    TextNode("another link", text_type_link, "https://blog.boot.dev"),
+    TextNode(" with text that follows", text_type_text),
+])
+print(new_nodes)
